81273ea1d69f3a2a0daeb673418e21ddfbf37b04
Thiago0013 <ianthigo13@gmail.com>
2025-11-25 11:20:18 -0300

    Arrumando um pequeno problema de DTO

--- src/main/java/com/example/agendacontatos/dto/ContatoDTO.java
+++ src/main/java/com/example/agendacontatos/dto/ContatoDTO.java
@@ -2,4 +2,4 @@ package com.example.agendacontatos.dto;
 
 import com.example.agendacontatos.model.Usuario;
 
-public record ContatoDTO(String nome, String email, String numero, Long idUsuario){}
+public record ContatoDTO(String nome, String email, String telefone, Long idUsuario){}
